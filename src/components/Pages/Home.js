73a4b451fc2067a13e34be87255f1979e64b2487
Meyssa <fm_zouambi@esi.dz>
2020-03-24 00:26:14 +0100

    added portfolio section

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,11 @@
 import React , {Component} from "react";
 import Header  from "../Common/Header";
 import image from "../assets/img/header-bg.jpg"
+
+//Sections
 import Services from "../Common/Services";
+import Portfolio from "../Common/Portfolio";
+
 
 class Home extends Component {
     render(){
@@ -16,6 +20,7 @@ class Home extends Component {
                     image={image}
                 ></Header>
                 <Services/>
+                <Portfolio/>
             </div>
         )
     }
